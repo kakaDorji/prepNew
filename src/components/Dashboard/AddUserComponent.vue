f6bf40a5571a94e90ff24463db4833a0670b4b87
karson <karsonam458@gmail.com>
2025-07-24 11:04:19 +0600

    name issuse solved

--- src/components/Dashboard/AddUserComponent.vue
+++ src/components/Dashboard/AddUserComponent.vue
@@ -57,7 +57,7 @@
               <div>
                 <label for="full_name" class="block text-sm font-medium text-gray-700">Full name</label>
                 <div class="mt-1">
-                  <input v-model="form.full_name" type="text" id="full_name" name="full_name" required
+                  <input v-model="form.fullname" type="text" id="full_name" name="fullname" required
                     class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
                 </div>
               </div>
@@ -139,7 +139,7 @@ import axios from "axios";
 const form = ref({
   username: "",
   password: "",
-  full_name: "",
+  fullname: "",
   designation: "",
   prep_location: "",
   role: "",
